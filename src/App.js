8ab3fd608fa34eab580adb020d75e53f479a30f9
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-10-24 13:04:55 -0500

    [95]: Creando primer componente utilizando useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  //useState retorna 2 funciones
+  // state actual = this.state; // citas
+  // función que actualiza el state = this.setState(); // guardarCita
+  const [citas, guardarCita ] = useState([]);
+  console.log(citas)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>hola</h1>
+  )
 }
 
 export default App;
